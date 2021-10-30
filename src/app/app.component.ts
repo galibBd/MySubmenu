2a3378b2ce744e2c7c2635784c1bf5e123c2e906
galibBd <galibmohammad47@gmail.com>
2021-10-30 17:11:43 +0600

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'mySubmenu';
+  name: string;
+  title: String;
+  menuList: any;
+  selected: any;
+  constructor() {
+    this.title = "Collapsible menu - Angular 2";
+    this.menuList = [{
+      "name": "Angular",
+      "subMenu": ["Anguler 1", "Angular 2"]
+    }, {
+      "name": "Javascript",
+      "subMenu": ["Jquery", "Ajax"]
+    }, {
+      "name": "Bootstrap",
+      "subMenu": ["BootStrap 2", "BootStrap 3"]
+    }]
+
+  }
+  select(item) {
+    this.selected = (this.selected === item ? null : item);
+  }
+  isActive(item) {
+    return this.selected === item;
+  }
 }
